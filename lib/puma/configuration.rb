203da953e990b93e69eabac0a1fbb52a7d124614
MSP-Greg <Greg.mpls@gmail.com>
2024-11-28 10:29:29 -0600

    Update configuration.rb

--- lib/puma/configuration.rb
+++ lib/puma/configuration.rb
@@ -140,11 +140,9 @@ module Puma
       io_selector_backend: :auto,
       log_requests: false,
       logger: STDOUT,
-      # How many requests to attempt inline before sending a client back to
-      # the reactor to be subject to normal ordering. The idea here is that
-      # we amortize the cost of going back to the reactor for a well behaved
-      # but very "greedy" client across 10 requests. This prevents a not
-      # well behaved client from monopolizing the thread forever.
+      # Limits how many requests a keep alive connection can make.
+      # The connection will be closed after it reaches `max_fast_inline`
+      # requests.
       max_fast_inline: 10,
       max_threads: Puma.mri? ? 5 : 16,
       min_threads: 0,
