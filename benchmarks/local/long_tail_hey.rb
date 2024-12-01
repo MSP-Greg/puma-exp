a26b8796d89558c5292bf7aef28f2ed68271a881
MSP-Greg <Greg.mpls@gmail.com>
2024-11-30 20:41:35 -0600

    CI: update hey benchmark files to output Puma stats info

--- benchmarks/local/long_tail_hey.rb
+++ benchmarks/local/long_tail_hey.rb
@@ -32,7 +32,9 @@ module TestPuma
 
     HEY = ENV.fetch('HEY', 'hey')
 
-    CONNECTION_MULT = [1.0, 1.5, 2.0, 3.0, 4.0]
+    # CONNECTION_MULT = [1.0, 1.5, 2.0, 3.0, 4.0]
+    CONNECTION_MULT = [1.0, 1.5, 2.0, 3.0, 4.0].reverse
+    CONNECTION_REQ = []
 
     def run
       time_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
@@ -43,18 +45,20 @@ module TestPuma
       summaries = Hash.new { |h,k| h[k] = {} }
 
       @stats_data = {}
-
-      @hey_data = {}
+      @hey_data   = {}
 
       STDOUT.syswrite "\n"
 
       cpu_qty = ENV['HEY_CPUS']
       hey_cpus = cpu_qty ? "-cpus #{cpu_qty} " : ""
 
+      @ka = @no_keep_alive ? "-disable-keepalive" : ""
+
       CONNECTION_MULT.each do |mult|
         workers = @workers || 1
         connections = (mult * @threads * workers).round
-        @ka = @no_keep_alive ? "-disable-keepalive" : ""
+
+      CONNECTION_REQ << connections
 
         hey_cmd = %Q[#{HEY} -c #{format '%3d', connections} -n #{format '%5d', connections * @req_per_connection} #{hey_cpus}#{@ka} #{@wrk_bind_str}/sleep#{@dly_app}]
 
@@ -76,93 +80,103 @@ module TestPuma
       STDOUT.syswrite format("\n%2d:%02d Total Time\n", (running_time/60).to_i, running_time % 60)
     end
 
-    # Prints parsed data of each wrk run. Similar to:
-    # ```
-    # Type   req/sec    50%     75%     90%     99%    100%  Resp Size
-    # ─────────────────────────────────────────────────────────────────    1kB
-    # array   13760    0.74    2.51    5.22    7.76   11.18      2797
-    # ```
-    #
-    # @param summaries [Hash]
-    #
     def run_summaries
-      if @errors
-#        puts "\n#{label}   req/sec    50%     75%     90%     99%    100%  Resp Size  Errors"
-#        desc_width = 83
-      else
-        worker_str = @workers.nil? ? '   ' : "-w#{@workers}"
-        worker_div = 100/@workers.to_f
+      STDOUT.syswrite "\n\n"
+      worker_str = @workers.nil? ? '   ' : "-w#{@workers}"
+      worker_div = 100/@workers.to_f
 
-        str = "\n\n" \
-          "Branch: #{%x[git branch --show-current].strip.ljust 16} " \
+      # Below are lines used in data logging
+      @desc_line = "Branch: #{%x[git branch --show-current].strip.ljust 16} " \
           "Puma: #{worker_str.ljust 4} -t#{@threads}:#{@threads}  dly #{@dly_app}"
-
-        str_len = str.length
-
-        str << if @workers.nil? || @workers < 2
-          "\n#{@ka.ljust 18}       ─────────────────────────── Latency ───────────────────────────\n" \
-          "Mult/Conn     req/sec      10%     25%     50%     75%     90%     95%     99%    100%\n"
-        else
-          wid_1 = [2, 5, 7, 10, 12, 15, 17]
-          ind_1 = wid_1[@workers - 2]
-
-          # workers   3  4   5   6   7   8
-          wid_2 = [3, 5, 8, 10, 13, 15, 18]
-          wid_3 = [2, 5, 7, 10, 12, 15, 17]
-          ind_2 = wid_2[@workers - 2]
-          ind_3 = wid_3[@workers - 2]
-
-
-          "#{' ' * (93 - str_len)}#{'─' * ind_1} Worker Request Info #{'─' * ind_1}\n" \
-          "#{@ka.ljust 18   }       ─────────────────────────── Latency ───────────────────────────    Std" \
-            "#{' ' * ind_2}% deviation#{' ' * ind_3}Total\n" \
-          "Mult/Conn     req/sec      10%     25%     50%     75%     90%     95%     99%    100%      Dev" \
-            "#{' ' * ind_2}from #{format '%5.2f', worker_div}%#{' ' * ind_3} Reqs\n"
-        end
-
-        STDOUT.syswrite str
+      @hey_info_line = "Mult/Conn  requests"
+      @hey_run_data = []
+      @hey_data.each do |k, data|
+        @hey_run_data[k] = "#{data[:mult]} #{k.to_s.rjust 3}"
       end
+
+      STDOUT.syswrite summary_hey_latency
+      STDOUT.syswrite summary_puma_stats
+    end
+
+    def summary_hey_latency
+      str = @desc_line.dup
+      str_len = str.length
+
+      str << "\n#{@ka.ljust 30}  ──────────────────────── Hey Latency ─────────────────────────\n" \
+        "#{@hey_info_line}  req/sec      10%     25%     50%     75%     90%     95%     99%    100%\n"
 
       @hey_data.each do |k, data|
-        STDOUT.syswrite "#{data[:mult]} #{k.to_s.rjust 3} #{data[:rps].to_s.rjust 12}  "
-        data[:latency].each { |pc, time| STDOUT.syswrite "#{time.to_s.rjust 8}" }
+        str << format("#{@hey_run_data[k]}   %6d   %8.2f  ", data[:requests], data[:rps])
+        data[:latency].each { |pc, time| str << "#{time.to_s.rjust 8}" }
+        str << "\n"
+      end
+      str << "\n"
+    end
 
-        if (@workers || 0)> 1
-          # print worker request info from stats
-          if stats = @stats_data[k]
-            div = k * @req_per_connection/100.to_f
-            ttl_req = 0
-            req = []
-            stats.each do |_,v|
-              t = v[:requests]
-              ttl_req += t
-              req << t/div.to_f
-            end
-            n = req.length.to_f
+    def summary_puma_stats
+      str = @desc_line.dup
+      str_len = str.length
+      if (@workers || 0) > 1
+        # used for 'Worker Request Info' centering
+        # worker 2  3  4  5   6   7   8
+        wid_1 = [2, 3, 5, 8, 11, 14, 17]
+        ind_1 = wid_1[@workers - 2]
 
-            sq_sum = 0
-            sum = 0
-            req.each do |i|
-              sq_sum += i**2
-              sum += i
-            end
-            var = (sq_sum - sum**2/n)/n
-            STDOUT.syswrite format('  %7.3f', Math.sqrt(var))
+        # used for '% deviation' centering
+        # worker 2  3   4   5   6   7   8
+        wid_2 = [4, 7, 10, 13, 16, 19, 22]
+        ind_2 = wid_2[@workers - 2]
 
-            # req.sort.each { |r| STDOUT.syswrite format(' %5.2f', r) }
-            percents = req.sort.map do |r|
-              percent = 100 * (r - worker_div)/worker_div
-              fmt = percent.between?(-9.9, 9.9) ? ' %4.1f' : ' %4d'
-              format fmt, percent
-            end.join
+        str << "#{' ' * (57 - str_len)}#{'─' * ind_1} Worker Request Info #{'─' * ind_1}\n"
 
-            STDOUT.syswrite "  #{percents}  #{format ' %5d', ttl_req}"
+        str << "#{@ka.ljust 23           }  ── Reactor ──   ── Backlog ──" \
+          "    Std #{' ' * ind_2}% deviation\n"
+
+        str << "#{@hey_info_line.ljust 23}   Min     Max     Min     Max " \
+          "    Dev #{' ' * ind_2}from #{format '%5.2f', 100/@workers.to_f }%\n"
+
+        CONNECTION_REQ.each do |k|
+          backlog_max = []
+          reactor_max = []
+          requests = []
+          hsh = @stats_data[k]
+
+          hsh.each do |k, v|
+            backlog_max << v[:backlog_max]
+            reactor_max << v[:reactor_max]
+            requests << v[:requests]
           end
-          STDOUT.syswrite "\n"
-        else
-          STDOUT.syswrite "\n"
+
+          str << format("#{@hey_run_data[k]}   %6d       %4d   %5d   %5d   %5d",
+            requests.sum, reactor_max.min, reactor_max.max, backlog_max.min, backlog_max.max)
+
+          # std dev calc
+          n = requests.length.to_f
+          sq_sum = 0
+          sum = 0
+          requests.each do |i|
+            sq_sum += i**2
+            sum += i
+          end
+          var = (sq_sum - sum**2/n)/n
+
+          div = k * @req_per_connection/@workers.to_f
+          percents = requests.sort.map do |r|
+            percent = 100.0 * (r - div)/div
+            format ' %5.1f', percent
+          end.join
+
+          str << format("   %7.2f  #{percents}\n", Math.sqrt(var))
+        end
+      else
+        str << "\n#{@ka.ljust 23}  ────── Max ─────\n"
+        str << "#{@hey_info_line.ljust 23}  Reactor  Backlog\n"
+        CONNECTION_REQ.each do |k|
+          hsh = @stats_data[k]
+          str << format("#{@hey_run_data[k]}   %6d         %3d      %3d\n", hsh[:requests], hsh[:reactor_max], hsh[:backlog_max])
         end
       end
+      str << "\n\n"
     end
 
     def puts(*ary)
