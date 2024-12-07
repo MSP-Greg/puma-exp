4e727e51ff138842b5ce239b326a97c566bc24c7
MSP-Greg <Greg.mpls@gmail.com>
2024-12-07 13:27:10 -0600

    long_tail_hey-0-2-4-8.sh - add total time, app time/dly argument

--- benchmarks/local/long_tail_hey-0-2-4-8.sh
+++ benchmarks/local/long_tail_hey-0-2-4-8.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 # benchmarks/local/long_tail_hey-0-2-4-8.sh
-
+#
+# takes one argument which is app delay, default to 0.2
+#
 # see comments in long_tail_hey.rb
 
-benchmarks/local/long_tail_hey.sh -t5:5 -R100 -d0.2
+if [[ -n $1 ]]; then
+  DLY=$1
+else
+  DLY=0.2
+fi
+
+SECONDS=0
+
+benchmarks/local/long_tail_hey.sh -t5:5 -R100 -d$DLY
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w2 -t5:5 -R100 -d0.2
+benchmarks/local/long_tail_hey.sh -w2 -t5:5 -R100 -d$DLY
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w4 -t5:5 -R100 -d0.2
+benchmarks/local/long_tail_hey.sh -w4 -t5:5 -R100 -d$DLY
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w8 -t5:5 -R100 -d0.2
+benchmarks/local/long_tail_hey.sh -w8 -t5:5 -R100 -d$DLY
 sleep 2s
-benchmarks/local/long_tail_hey.sh -t5:5 -R100 -d0.2 -k
+benchmarks/local/long_tail_hey.sh -t5:5 -R100 -d$DLY -k
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w2 -t5:5 -R100 -d0.2 -k
+benchmarks/local/long_tail_hey.sh -w2 -t5:5 -R100 -d$DLY -k
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w4 -t5:5 -R100 -d0.2 -k
+benchmarks/local/long_tail_hey.sh -w4 -t5:5 -R100 -d$DLY -k
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w8 -t5:5 -R100 -d0.2 -k
+benchmarks/local/long_tail_hey.sh -w8 -t5:5 -R100 -d$DLY -k
 sleep 2s
+
+TZ=UTC0 printf '\n%(%H:%M:%S)T All Total Time\n' $SECONDS
