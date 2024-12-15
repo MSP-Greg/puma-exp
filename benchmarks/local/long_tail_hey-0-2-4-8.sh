f5b370db8bdac478e9edeacb81e61f6c403104c7
MSP-Greg <Greg.mpls@gmail.com>
2024-12-15 13:07:03 -0600

    Update long_tail_hey-0-2-4-8.sh

--- benchmarks/local/long_tail_hey-0-2-4-8.sh
+++ benchmarks/local/long_tail_hey-0-2-4-8.sh
@@ -12,23 +12,27 @@ else
   DLY=0.2
 fi
 
+THREADS=5:5
+REQS_PER_CONN=100
+
 SECONDS=0
 
-benchmarks/local/long_tail_hey.sh -t5:5 -R100 -d$DLY
+benchmarks/local/long_tail_hey.sh     -t$THREADS -R$REQS_PER_CONN -d$DLY
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w2 -t5:5 -R100 -d$DLY
+benchmarks/local/long_tail_hey.sh -w2 -t$THREADS -R$REQS_PER_CONN -d$DLY
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w4 -t5:5 -R100 -d$DLY
+benchmarks/local/long_tail_hey.sh -w4 -t$THREADS -R$REQS_PER_CONN -d$DLY
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w8 -t5:5 -R100 -d$DLY
+benchmarks/local/long_tail_hey.sh -w8 -t$THREADS -R$REQS_PER_CONN -d$DLY
 sleep 2s
-benchmarks/local/long_tail_hey.sh -t5:5 -R100 -d$DLY -k
+# disable keep-alive
+benchmarks/local/long_tail_hey.sh     -t$THREADS -R$REQS_PER_CONN -d$DLY -k
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w2 -t5:5 -R100 -d$DLY -k
+benchmarks/local/long_tail_hey.sh -w2 -t$THREADS -R$REQS_PER_CONN -d$DLY -k
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w4 -t5:5 -R100 -d$DLY -k
+benchmarks/local/long_tail_hey.sh -w4 -t$THREADS -R$REQS_PER_CONN -d$DLY -k
 sleep 2s
-benchmarks/local/long_tail_hey.sh -w8 -t5:5 -R100 -d$DLY -k
+benchmarks/local/long_tail_hey.sh -w8 -t$THREADS -R$REQS_PER_CONN -d$DLY -k
 sleep 2s
 
 TZ=UTC0 printf '\n%(%H:%M:%S)T All Total Time\n' $SECONDS
